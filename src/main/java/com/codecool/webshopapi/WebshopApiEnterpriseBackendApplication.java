a78ca4a94948128d7f114611a4ce9210ac408c85
zsszakal <zs.szakal@outlook.com>
2021-07-21 18:06:35 +0200

    Add project and .gitignore

---
+++ src/main/java/com/codecool/webshopapi/WebshopApiEnterpriseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.webshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApiEnterpriseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopApiEnterpriseBackendApplication.class, args);
+    }
+
+}
